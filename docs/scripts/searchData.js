d0fd62318b4389deec3c85d027b75f362a4aace2
Roland Tritsch <roland@tritsch.email>
2025-12-16 12:33:50 +0000

    Commit docs (to publish it with pages)

---
+++ docs/scripts/searchData.js
@@ -0,0 +1,5 @@
+pages = [{"l":"index.html#","e":false,"i":"","n":"app","t":"app","d":"","k":"static","x":""},
+{"l":"$lessempty$greater$/Main$.html#","e":false,"i":"","n":"Main","t":"Main extends ZIOAppDefault","d":"","k":"object","x":""},
+{"l":"$lessempty$greater$/Main$.html#main-913","e":false,"i":"","n":"main","t":"main(args0: Array[String]): Unit","d":"Main","k":"def","x":""},
+{"l":"$lessempty$greater$/Main$.html#program-0","e":false,"i":"","n":"program","t":"program: ZIO[Any, Nothing, Unit]","d":"Main","k":"val","x":""},
+{"l":"$lessempty$greater$/Main$.html#run-0","e":false,"i":"","n":"run","t":"run: ZIO[Environment & ZIOAppArgs & Scope, Any, Any]","d":"Main","k":"def","x":""}];
