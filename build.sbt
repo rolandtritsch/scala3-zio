6766d8707f211e8b87ebfbd1bc1a369436601beb
Roland Tritsch <roland@tritsch.email>
2025-12-19 06:38:13 +0000

    Implement shutdown. Make ctrl-c work.

--- build.sbt
+++ build.sbt
@@ -5,6 +5,7 @@ ThisBuild / semanticdbVersion := scalafixSemanticdb.revision
 
 lazy val root = (project in file(".")).settings(
   name := "scala3-zio-template",
+  run / fork := true,
   libraryDependencies ++= Seq(
     "ch.qos.logback" % "logback-classic" % "1.4.14",
     "dev.zio" %% "zio-http" % "3.6.0",
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -38,56 +38,18 @@ object Main extends ZIOAppDefault:
       ZIO.logInfo("Server shutdown completed successfully") *>
       ZIO.logInfo("Exiting application...")
 
-  // $COVERAGE-OFF$ JVM shutdown hooks cannot be tested in unit tests
-  private def registerShutdownHook(
-      shutdownPromise: Promise[Nothing, Unit]
-  ): UIO[Unit] =
-    ZIO.logInfo("Registering JVM shutdown hook for SIGTERM/SIGINT...") *>
-      ZIO.succeed {
-        java
-          .lang
-          .Runtime
-          .getRuntime
-          .addShutdownHook(
-            new Thread(new Runnable {
-              def run(): Unit = {
-                // Complete the promise using fire-and-forget
-                // This avoids blocking on the runtime during JVM shutdown
-                zio
-                  .Unsafe
-                  .unsafe { implicit u =>
-                    zio.Runtime.default.unsafe.fork(shutdownPromise.succeed(()))
-                    ()
-                  }
-              }
-            })
-          )
-      }
-  // $COVERAGE-ON$
-
   def run =
     (for {
       shutdownPromise <- Promise.make[Nothing, Unit]
-      // Only register shutdown hook in production (not when running via sbt)
-      // In dev mode (sbt run), CTRL-C interrupts the fiber directly
-      _ <- ZIO
-        .succeed(java.lang.System.getProperty("sun.java.command", ""))
-        .flatMap { (cmd: String) =>
-          if (cmd.contains("sbt.Run")) {
-            ZIO.logInfo(
-              "Running in SBT mode - CTRL-C will interrupt fiber directly"
-            )
-          } else {
-            registerShutdownHook(shutdownPromise)
-          }
-        }
       allRoutes = routes(shutdownPromise)
       _ <- ZIO.logInfo("Starting server on port 8080...")
-      _ <- Server
+      server <- Server
         .serve(allRoutes)
-        .race(shutdownPromise.await *> ZIO.logInfo("Shutdown initiated..."))
+        .race(shutdownPromise.await *> performGracefulShutdown)
+        .onInterrupt(ZIO.logInfo("Interrupt received, shutting down...") *> performGracefulShutdown)
+        .fork
+      _ <- server.join
     } yield ())
-      .ensuring(performGracefulShutdown)
       .provide(
         ZLayer.succeed(serverConfig),
         Server.live
